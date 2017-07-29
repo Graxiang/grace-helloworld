92f3d897bc4587993f3b2e67069fe630ef35d56d
Graxiang <marygrace.puyot@jmc.edu.ph>
2017-07-29 11:57:54 +0800

    add my add-button

--- graceproject/graceproject/Form1.cs
+++ graceproject/graceproject/Form1.cs
@@ -16,5 +16,10 @@ namespace graceproject
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
